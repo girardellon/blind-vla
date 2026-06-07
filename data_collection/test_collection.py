a620daa3a0b315ab1f8da85acb48b829d7b6e23e
girardellon <gira.girardello@gmail.com>
2026-06-07 12:17:23 +0200

    Updated collection pipeline

--- data_collection/test_collection.py
+++ data_collection/test_collection.py
@@ -21,47 +21,15 @@ p.connect(p.GUI)
 
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-base_path = os.path.join(
-    os.getcwd(),
-    "assets",
-)
-
-print("cwd =", os.getcwd())
-print("exists =", os.path.exists("objects/sphere_small.urdf"))
-p.setAdditionalSearchPath(base_path)
-
 p.resetSimulation()
 p.setGravity(0,0,-9.81)
 
 robot_id = p.loadURDF(
-    "franka_panda/panda_digit.urdf",
+    "assets/franka_panda/panda_digit.urdf",
     [0,0,0],
     useFixedBase=True
 )
 
-sensor = tacto.Sensor(width=224, height=224)
-
-sensor.add_camera(robot_id, [10])   # digit_left_link
-sensor.add_camera(robot_id, [12])   # digit_right_link
-
-# --------------------------------------------------
-# print all links
-# --------------------------------------------------
-
-link_map = {}
-
-print("\nLINKS\n")
-
-for i in range(p.getNumJoints(robot_id)):
-    info = p.getJointInfo(robot_id, i)
-
-    link_name = info[12].decode()
-
-    link_map[link_name] = i
-
-    print(i, link_name)
-
-print("\nMAP =", link_map)
 
 # --------------------------------------------------
 # table
@@ -85,87 +53,227 @@ table_id = p.createMultiBody(
 )
 
 # --------------------------------------------------
-# cube
+# objects
 # --------------------------------------------------
 
-# cube_collision = p.createCollisionShape(
-#     p.GEOM_BOX,
-#     halfExtents=[0.025]*3
-# )
+OBJECTS = [
+    {
+        "name": "cage",
+        "urdf": "assets/objects/cage.urdf",
+        "scale": 0.5,
+    },
+    {
+        "name": "decorative_ball",
+        "urdf": "assets/objects/decorative_ball.urdf",
+        "scale": 0.4,
+    },
+    {
+        "name": "sphere_small",
+        "urdf": "assets/objects/sphere_small.urdf",
+        "scale": 0.5
+    },
+    {
+        "name": "abstract_ball",
+        "urdf": "assets/objects/abstract_ball.urdf",
+        "scale": 0.4,
+    },
+    {
+        "name": "disco_ball",
+        "urdf": "assets/objects/disco_ball.urdf",
+        "scale": 0.4,
+    },
+    {
+        "name": "cube",
+        "urdf": "assets/objects/cube_small.urdf",
+        "scale": 0.5
+    },
+    {
+        "name": "wood_dice",
+        "urdf": "assets/objects/wood_dice.urdf",
+        "scale": 0.5,
+    },
+    {
+        "name": "greek_dice",
+        "urdf": "assets/objects/greek_dice.urdf",
+        "scale": 0.5,
+    }
+]
 
-# cube_visual = p.createVisualShape(
-#     p.GEOM_BOX,
-#     halfExtents=[0.025]*3
-# )
+# --------------------------------------------------
+# object definition
+# --------------------------------------------------
 
-# cube_id = p.createMultiBody(
-#     baseMass=0.1,
-#     baseCollisionShapeIndex=cube_collision,
-#     baseVisualShapeIndex=cube_visual,
-#     basePosition=[0.55,0.0,0.3]
-# )
+def spawn_object(obj_cfg):
 
-# cube_id = p.loadURDF(
-#     "objects/cube_small.urdf",
-#     [0.55, 0.0, 0.3],
-#     globalScaling=0.5
-# )
+    x = 0.55 + np.random.uniform(-0.01, 0.01)
+    y = np.random.uniform(-0.01, 0.01)
 
-# cube_body = TactoBody(
-#     cube_id,
-#     "local_cube.urdf",
-#     global_scaling=0.05
-# )
+    # yaw = np.random.uniform(-np.pi, np.pi)
+    yaw = 0
 
-# sphere_id = p.loadURDF(
-#     "objects/sphere_small.urdf",
-#     [0.55, 0.0, 0.3],
-#     globalScaling=0.5
-# )
+    obj_id = p.loadURDF(
+        obj_cfg["urdf"],
+        [x, y, 0.3],
+        p.getQuaternionFromEuler([0, 0, yaw]),
+        globalScaling=obj_cfg["scale"]
+    )
 
-# sphere_body = TactoBody(
-#     sphere_id,
-#     "assets/objects/sphere_small.urdf",
-#     global_scaling=0.5
-# )
+    body = TactoBody(
+        obj_id,
+        obj_cfg["urdf"],
+        global_scaling=obj_cfg["scale"]
+    )
 
-# cone_id = p.loadURDF(
-#     "objects/cone.urdf",
-#     [0.55, 0.0, 0.3],
-#     globalScaling=0.5
-# )
+    return obj_id, body
 
-# cone_body = TactoBody(
-#     cone_id,
-#     "assets/objects/cone.urdf",
-#     global_scaling=0.5
-# )
+# --------------------------------------------------
+# collect external camera
+# --------------------------------------------------
 
-# ball_id = p.loadURDF(
-#     "objects/abstract_ball.urdf",
-#     [0.55, 0.0, 0.3],
-#     globalScaling=0.4
-# )
+def capture_external_camera(
+    robot_id,
+    hand_link,
+    object_id
+):
 
-# ball_body = TactoBody(
-#     ball_id,
-#     "assets/objects/abstract_ball.urdf",
-#     global_scaling=0.4
-# )
+    state = p.getLinkState(
+        robot_id,
+        hand_link,
+        computeForwardKinematics=True
+    )
 
-ball_id = p.loadURDF(
-    "objects/sphere_small.urdf",
-    [0.55, 0.0, 0.3],
-    globalScaling=0.5
-)
+    hand_pos = np.array(state[4])
+    hand_quat = state[5]
 
-ball_body = TactoBody(
-    ball_id,
-    "assets/objects/sphere_small.urdf",
-    global_scaling=0.5
-)
+    obj_pos, _ = p.getBasePositionAndOrientation(
+        object_id
+    )
 
-sensor.add_body(ball_body)
+    R = np.array(
+        p.getMatrixFromQuaternion(hand_quat)
+    ).reshape(3,3)
+
+    forward = R[:,0]
+
+    cam_pos = hand_pos + 0.8 * forward
+
+    view = p.computeViewMatrix(
+        cameraEyePosition=cam_pos,
+        cameraTargetPosition=obj_pos,
+        cameraUpVector=-R[:,2]
+    )
+
+    proj = p.computeProjectionMatrixFOV(
+        fov=5,
+        aspect=640/480,
+        nearVal=0.01,
+        farVal=2.0
+    )
+
+    w,h,rgba,depth,seg = p.getCameraImage(
+        width=640,
+        height=480,
+        viewMatrix=view,
+        projectionMatrix=proj,
+        # renderer=p.ER_BULLET_HARDWARE_OPENGL
+        renderer=p.ER_TINY_RENDERER
+    )
+
+    rgb = np.reshape(
+        rgba,
+        (h,w,4)
+    )[:,:,:3]
+
+    return cv2.cvtColor(
+        rgb,
+        cv2.COLOR_RGB2BGR
+    )
+
+# def capture_external_camera(robot_id, hand_link, object_id):
+
+#     obj_pos, _ = p.getBasePositionAndOrientation(
+#         object_id
+#     )
+
+#     cam_pos = [
+#         obj_pos[0] + 0.25,
+#         obj_pos[1] + 0.25,
+#         obj_pos[2] + 0.15
+#     ]
+
+#     view = p.computeViewMatrix(
+#         cameraEyePosition=cam_pos,
+#         cameraTargetPosition=obj_pos,
+#         cameraUpVector=[0,0,1]
+#     )
+
+#     proj = p.computeProjectionMatrixFOV(
+#         fov=60,
+#         aspect=640/480,
+#         nearVal=0.01,
+#         farVal=2.0
+#     )
+
+#     w,h,rgba,depth,seg = p.getCameraImage(
+#         width=640,
+#         height=480,
+#         viewMatrix=view,
+#         projectionMatrix=proj,
+#         renderer=p.ER_TINY_RENDERER
+#         # renderer=p.ER_BULLET_HARDWARE_OPENGL
+#     )
+
+#     rgb = np.reshape(rgba, (h,w,4))[:,:,:3]
+
+#     return cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+
+# --------------------------------------------------
+# save samples
+# --------------------------------------------------
+
+def save_sample(
+    sample_dir,
+    left_digit,
+    right_digit,
+    external_rgb,
+    object_name,
+    object_pose,
+    joint_positions,
+    contact_forces
+):
+
+    os.makedirs(sample_dir, exist_ok=True)
+
+    cv2.imwrite(
+        os.path.join(sample_dir, "digit_left.png"),
+        left_digit
+    )
+
+    cv2.imwrite(
+        os.path.join(sample_dir, "digit_right.png"),
+        right_digit
+    )
+
+    cv2.imwrite(
+        os.path.join(sample_dir, "external.png"),
+        external_rgb
+    )
+
+    np.savez(
+        os.path.join(sample_dir, "metadata.npz"),
+        object_name=object_name,
+
+        object_position=np.array(object_pose[0]),
+        object_quaternion=np.array(object_pose[1]),
+
+        joint_positions=np.array(joint_positions),
+
+        contact_forces=np.array(contact_forces)
+    )
+
+# --------------------------------------------------
+# home pose
+# --------------------------------------------------
 
 home_pose = [
     0.085,
@@ -176,8 +284,6 @@ home_pose = [
     1.885,
     0.785
 ]
-
-print("Going to HOME pose...")
 
 for _ in range(300):
     for j in range(7):
@@ -190,9 +296,20 @@ for _ in range(300):
         )
     p.stepSimulation()
     time.sleep(1/240)
+
 # --------------------------------------------------
 # choose ee link
 # --------------------------------------------------
+
+link_map = {}
+
+for i in range(p.getNumJoints(robot_id)):
+    info = p.getJointInfo(robot_id, i)
+
+    link_name = info[12].decode()
+
+    link_map[link_name] = i
+
 
 ee_link = link_map["panda_grasptarget"]
 
@@ -211,266 +328,262 @@ orientation = p.getQuaternionFromEuler([math.pi, 0, 0])
 
 ll = [-2.9, -1.8, -2.9, -3.0, -2.9, -0.1, -2.9]
 ul = [ 2.9,  1.8,  2.9,  0.0,  2.9,  3.7,  2.9]
-
 jr = [u-l for l,u in zip(ll, ul)]
-
-
-rp = [
-    0.134,
-    -0.042,
-    -0.049,
-    -1.894,
-    -0.139,
-    1.761,
-    0.076
-]
-
-OPENING = 0.04
+rp = [ 0.134, -0.042, -0.049, -1.894, -0.139, 1.761, 0.076]
 
 grasp_target = [0.55, 0.0, 0.31]
 
-while True:
-    joint_poses = p.calculateInverseKinematics(
-        robot_id,
-        ee_link,
-        target,
-        orientation,
-        lowerLimits=ll,
-        upperLimits=ul,
-        jointRanges=jr,
-        restPoses=rp
-        # jointDamping=[0.2]*7
+# --------------------------------------------------
+# main collection loop
+# --------------------------------------------------
+
+
+DATASET_ROOT = "dataset"
+SAMPLES_PER_OBJECT = 10
+
+os.makedirs(
+    DATASET_ROOT,
+    exist_ok=True
+)
+
+sample_counter = 0
+
+for obj_cfg in OBJECTS:
+
+    print(
+        f"\nCollecting {obj_cfg['name']}"
     )
 
-    for j in range(7):
-        p.setJointMotorControl2(
-            robot_id,
-            j,
-            p.POSITION_CONTROL,
-            targetPosition=joint_poses[j],
-            force=200
-        )
+    object_folder = os.path.join(
+        DATASET_ROOT,
+        obj_cfg["name"]
+    )
 
+    os.makedirs(
+        object_folder,
+        exist_ok=True
+    )
 
+    for sample_idx in range(
+        SAMPLES_PER_OBJECT
+    ):
 
-    p.stepSimulation()
-    time.sleep(1/240)
-
-    for _ in range(240):
-        p.setJointMotorControl2(
-            robot_id,
-            9,
-            p.POSITION_CONTROL,
-            targetPosition=OPENING,
-            force=5
-        )
-
-        p.setJointMotorControl2(
-            robot_id,
-            11,
-            p.POSITION_CONTROL,
-            targetPosition=OPENING,
-            force=5
-        )
-
-        p.stepSimulation()
-        time.sleep(1/240)
-
-    for _ in range(400):
-
-        joint_poses = p.calculateInverseKinematics(
-            robot_id,
-            ee_link,
-            grasp_target,
-            orientation,
-            lowerLimits=ll,
-            upperLimits=ul,
-            jointRanges=jr,
-            restPoses=rp
-        )
-
-        for j in range(7):
-            p.setJointMotorControl2(
-                robot_id,
-                j,
-                p.POSITION_CONTROL,
-                targetPosition=joint_poses[j],
-                force=200
-            )
-
-        p.stepSimulation()
-        time.sleep(1/240)
-
-    for _ in range(300):
-
-        p.setJointMotorControl2(
-            robot_id,
-            9,
-            p.POSITION_CONTROL,
-            targetPosition=0.0,
-            force=5
-        )
-
-        p.setJointMotorControl2(
-            robot_id,
-            11,
-            p.POSITION_CONTROL,
-            targetPosition=0.0,
-            force=5
-        )
-
-        p.stepSimulation()
-        time.sleep(1/240)
-
-    contacts = p.getContactPoints(robot_id, ball_id)
-
-    print("contacts:", len(contacts))
-
-    for c in contacts:
         print(
-            "robot link:", c[3],
-            "normal force:", c[9]
+            f"sample {sample_idx+1}/10"
         )
-        
-    color, depth = sensor.render()
-    print(len(color))
 
-    while True:
-        p.stepSimulation()
 
-        sensor.update()
-        color, depth = sensor.render()
+        sensor = tacto.Sensor(width=224, height=224)
 
-        cv2.imshow("left_digit", color[0])
-        cv2.imshow("right_digit", color[1])
-        cv2.waitKey(1)
+        sensor.add_camera(robot_id, [10])   # digit_left_link
+        sensor.add_camera(robot_id, [12])   # digit_right_link
 
-        # view = p.computeViewMatrix(
-        #     cameraEyePosition=cam_pos,
-        #     cameraTargetPosition=camera_target,
-        #     cameraUpVector=up
+
+        for _ in range(300):
+            for j in range(7):
+                p.setJointMotorControl2(
+                    robot_id,
+                    j,
+                    p.POSITION_CONTROL,
+                    targetPosition=home_pose[j],
+                    force=200
+                )
+
+            p.stepSimulation()
+            time.sleep(1/240)
+
+        obj_id, body = spawn_object(
+            obj_cfg
+        )
+
+
+        print("Loaded:", obj_cfg["name"])
+        print(p.getVisualShapeData(obj_id))
+
+
+        aabb = p.getAABB(obj_id)
+
+            
+        # scale = 0.05 / max(
+        #     aabb[1][0] - aabb[0][0],
+        #     aabb[1][1] - aabb[0][1],
+        #     aabb[1][2] - aabb[0][2],
         # )
 
-        obj_pos, _ = p.getBasePositionAndOrientation(ball_id)
+        # p.resetBasePositionAndOrientation(
+        #     obj_id,
+        #     pos,
+        #     orn
+        # )
+        print("AABB size:",
+            np.array(aabb[1]) - np.array(aabb[0]))
+        print("visual:", p.getVisualShapeData(obj_id)[0][3])
 
-        hand_pos = np.array(state[4])
-        hand_quat = state[5]
 
-        R = np.array(
-            p.getMatrixFromQuaternion(hand_quat)
-        ).reshape(3,3)
+        sensor.add_body(body)
 
-        forward = R[:,0]
-        up = -R[:,2]
-
-        cam_pos = hand_pos + 0.8 * forward
-
-        camera_target = hand_pos - 0.01 * forward
-
-        view = p.computeViewMatrix(
-            cameraEyePosition=cam_pos,
-            cameraTargetPosition=obj_pos,
-            cameraUpVector=-R[:,2]
+        obj_pos, _ = (
+            p.getBasePositionAndOrientation(
+                obj_id
+            )
         )
 
-        proj = p.computeProjectionMatrixFOV(
-            fov=5,
-            aspect=640/480,
-            nearVal=0.01,
-            farVal=2.0
-        )
+        grasp_target = [
+            obj_pos[0],
+            obj_pos[1],
+            0.31
+        ]
 
-        w,h,rgba,depth,seg = p.getCameraImage(
-            width=640,
-            height=480,
-            viewMatrix=view,
-            projectionMatrix=proj,
-            renderer=p.ER_BULLET_HARDWARE_OPENGL
-        )
+        for _ in range(240):
+            p.stepSimulation()
+            time.sleep(1/240)
 
-        rgb = np.reshape(rgba, (h,w,4))[:,:,:3]
-        rgb = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+        # open gripper
 
-        cv2.imshow("external_camera", rgb)
+        for _ in range(240):
 
-    while True:
-
-        for j in range(7):
             p.setJointMotorControl2(
                 robot_id,
-                j,
+                9,
                 p.POSITION_CONTROL,
-                targetPosition=joint_poses[j],
-                force=200
+                targetPosition=0.04,
+                force=5
             )
 
-        p.setJointMotorControl2(
+            p.setJointMotorControl2(
+                robot_id,
+                11,
+                p.POSITION_CONTROL,
+                targetPosition=0.04,
+                force=5
+            )
+
+            p.stepSimulation()
+
+        # move to object
+
+        for _ in range(400):
+
+            joint_poses = (
+                p.calculateInverseKinematics(
+                    robot_id,
+                    ee_link,
+                    grasp_target,
+                    orientation,
+                    lowerLimits=ll,
+                    upperLimits=ul,
+                    jointRanges=jr,
+                    restPoses=rp
+                )
+            )
+
+            for j in range(7):
+
+                p.setJointMotorControl2(
+                    robot_id,
+                    j,
+                    p.POSITION_CONTROL,
+                    targetPosition=
+                    joint_poses[j],
+                    force=200
+                )
+
+            p.stepSimulation()
+
+        # close gripper
+
+        for _ in range(300):
+
+            p.setJointMotorControl2(
+                robot_id,
+                9,
+                p.POSITION_CONTROL,
+                targetPosition=0.0,
+                force=10
+            )
+
+            p.setJointMotorControl2(
+                robot_id,
+                11,
+                p.POSITION_CONTROL,
+                targetPosition=0.0,
+                force=10
+            )
+
+            p.stepSimulation()
+
+        # settle contact
+
+        for _ in range(50):
+
+            p.stepSimulation()
+
+        sensor.update()
+
+        color, depth = sensor.render()
+
+        left_digit = color[0]
+        right_digit = color[1]
+
+        external_rgb = capture_external_camera(
             robot_id,
-            9,
-            p.POSITION_CONTROL,
-            targetPosition=0.0,
-            force=100
+            hand_link,
+            obj_id
         )
 
-        p.setJointMotorControl2(
+        contacts = p.getContactPoints(
             robot_id,
-            11,
-            p.POSITION_CONTROL,
-            targetPosition=0.0,
-            force=100
+            obj_id
         )
 
-        p.stepSimulation()
-        time.sleep(1/240)
+        forces = [
+            [c[3], c[9]]
+            for c in contacts
+        ]
 
+        joints = [
+            p.getJointState(
+                robot_id,
+                j
+            )[0]
+            for j in range(7)
+        ]
 
-# stabilize physics
-for _ in range(500):
-    p.stepSimulation()
-    time.sleep(1/240)
+        pose = (
+            p.getBasePositionAndOrientation(
+                obj_id
+            )
+        )
 
+        sample_dir = os.path.join(
+            object_folder,
+            f"sample_{sample_idx:03d}"
+        )
 
+        for _ in range(120):
+            p.stepSimulation()
+            time.sleep(1/240)
 
-# # open fingers
-# p.resetJointState(robot_id, 8, 0.04)
-# p.resetJointState(robot_id, 9, 0.04)
+        save_sample(
+            sample_dir,
+            left_digit,
+            right_digit,
+            external_rgb,
+            obj_cfg["name"],
+            pose,
+            joints,
+            forces
+        )
 
-while True:
-    p.stepSimulation()
-
-    contacts = p.getContactPoints(
-        bodyA=robot_id,
-        bodyB=cube_id
-    )
-
-    if len(contacts):
-        print("CONTACTS:", len(contacts))
-
-    for i in range(7):
-        print(f"joint {i}: {joint_poses[i]:.3f}")
-
-    num_joints = p.getNumJoints(robot_id)
-
-    while True:
-        p.stepSimulation()
-
-        print("\nJOINT STATES:")
-
-        for j in range(num_joints):
-            state = p.getJointState(robot_id, j)
-            pos = state[0]
-            print(f"joint {j}: {pos:.3f}")
-
-        time.sleep(1/240)
-
-    contacts = p.getContactPoints(robot_id, table_id)
-
-    for c in contacts:
         print(
-            "link:",
-            c[3],
-            "normal force:",
-            c[9]
+            f"saved -> {sample_dir}"
         )
 
-    time.sleep(1/240)
+        p.removeBody(obj_id)
+
+        sample_counter += 1
+
+print(
+    f"\nDONE. Collected "
+    f"{sample_counter} samples."
+)
